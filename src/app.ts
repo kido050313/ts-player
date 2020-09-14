253387e4aeb3f6e4d4ce2de3f55230af22b61f15
kido <1059064141@qq.com>
2020-09-14 23:28:37 +0800

    init: webpack配置,项目基本配置

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+let a: number = 123;
+
+export default a;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpakPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: "./src/main.ts",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "main.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        open: true // 自定打开浏览器
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.ts$/,
+                use: ['ts-loader'],
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpakPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    mode: "development"
+}
+
+// module.exports = config;

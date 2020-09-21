e9ee795d0e7d1ee8af3752631c974e59f9367ce3
kido <1059064141@qq.com>
2020-09-22 00:03:38 +0800

    feat: 完成弹出层组件

--- src/components/popup/popup.ts
+++ src/components/popup/popup.ts
@@ -9,12 +9,14 @@ interface Ipopup {
     title?: string;
     pos?: string;
     mask?: boolean;
-    content?: () => void;
+    closeMask?: boolean;
+    content?: (content: HTMLElement) => void;
 }
 
 // 模版接口,规范组件开发
 interface Icomponent {
     tempContainer: HTMLElement;
+    mask: HTMLElement;
     init: () => void;
     template: () => void;
     handle: () => void;
@@ -26,6 +28,7 @@ function popup(options: Ipopup) {
 
 class Popup implements Icomponent {
     tempContainer;
+    mask;
     // 将形参转成对象属性: public || private 类修饰符
     constructor( private settings: Ipopup ) {
         this.settings = Object.assign({
@@ -34,6 +37,7 @@ class Popup implements Icomponent {
             title: '',
             pos: 'center',
             mask: true,
+            closeMask: true,
             content: function() {}
         }, this.settings);
         this.init();
@@ -41,18 +45,65 @@ class Popup implements Icomponent {
     // 初始化
     init() {
         this.template();
+        this.settings.mask && this.createMask();
+        this.handle();
+        this.handleMask();
+        this.contentCallback();
     }
     // 创建模版
     template() {
         this.tempContainer = document.createElement('div');
+        this.tempContainer.style.width = this.settings.width;
+        this.tempContainer.style.height = this.settings.height;
+        this.tempContainer.className = styles.popup;
         this.tempContainer.innerHTML = `
-            <h1 class="${styles.popup}">hello</h1>
+            <div class="${styles['popup-title']}">
+                <h3>${this.settings.title}</h3>
+                <i class="iconfont icon-guanbi"></i>
+            </div>
+            <div class="${styles['popup-content']}"></div>
         `;
         document.body.appendChild(this.tempContainer);
+        if (this.settings.pos === 'left') {
+            this.tempContainer.style.left = 0;
+            this.tempContainer.style.top = (window.innerHeight - this.tempContainer.offsetHeight) / 2 + 'px';
+        } 
+        else if (this.settings.pos === 'right') {
+            this.tempContainer.style.right = 0;
+            this.tempContainer.style.top = (window.innerHeight - this.tempContainer.offsetHeight) / 2 + 'px';
+        } else {
+            this.tempContainer.style.left = (window.innerWidth - this.tempContainer.offsetWidth) / 2 + 'px';
+            this.tempContainer.style.top = (window.innerHeight - this.tempContainer.offsetHeight) / 2 + 'px';
+        }
     }
     // 事件操作
     handle() {
-
+        const popupClose = this.tempContainer.querySelector(`.${styles['popup-title']}`);
+        popupClose.addEventListener('click', () => {
+            this.closePopup()
+        })
+    }
+    handleMask() {
+        const maskEl = document.body.querySelector(`.${styles.mask}`);
+        maskEl && maskEl.addEventListener('click', () => {
+            this.settings.closeMask && this.closePopup() 
+        })
+    }
+    closePopup() {
+        document.body.removeChild(this.tempContainer);
+        this.settings.mask && document.body.removeChild(this.mask);
+    }
+    createMask() {
+        this.mask = document.createElement('div');
+        this.mask.className = styles.mask;
+        this.mask.style.width = '100%';
+        console.log(document.body.offsetHeight)
+        this.mask.style.height = document.body.offsetHeight + 'px';
+        document.body.appendChild(this.mask)
+    }
+    contentCallback () {
+        const popupContent = this.tempContainer.querySelector(`.${styles['popup-content']}`);
+        this.settings.content(popupContent)
     }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ for(let i = 0; i < list.length; i++) {
     list[i].addEventListener('click', function() {
         let {url, title } = this.dataset;
         console.log(url, title)
-        popup({});
+        popup({
+            width: '880px',
+            height: '556px',
+            title,
+            pos: 'center',
+            mask: true,
+            closeMask: false,
+            content(elem) {
+                console.log(elem)
+            }
+        });
     })
 }
